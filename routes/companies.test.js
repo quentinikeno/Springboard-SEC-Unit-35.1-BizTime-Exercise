4ba7b5937a272ffc2a6b3eed5e478d6072680baa
Quentin Ikeno <ikenoq@gmail.com>
2022-05-17 13:54:26 -1000

    Add test for /companies GET.

---
+++ routes/companies.test.js
@@ -0,0 +1,38 @@
+// Tell Node that we're in test "mode"
+process.env.NODE_ENV = "test";
+
+const { setUncaughtExceptionCaptureCallback } = require("process");
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db");
+
+let testCompany;
+let testInvoice;
+
+beforeEach(async () => {
+	const company = await db.query(
+		"INSERT INTO companies VALUES ('test', 'Test Company', 'This is a VERY REAL company. Legit.') RETURNING *"
+	);
+	const invoice = await db.query(
+		"INSERT INTO invoices (comp_Code, amt, paid, paid_date) VALUES ('test', 100, false, null) RETURNING *"
+	);
+	testCompany = company.rows[0];
+	testInvoice = invoice.rows[0];
+});
+
+afterEach(async () => {
+	await db.query("DELETE FROM companies");
+	await db.query("DELETE FROM invoices");
+});
+
+afterAll(async () => {
+	await db.end();
+});
+
+describe("GET /companies", () => {
+	test("Get a object of companies", async () => {
+		const res = await request(app).get("/companies");
+		expect(res.statusCode).toBe(200);
+		expect(res.body).toEqual({ companies: [testCompany] });
+	});
+});
