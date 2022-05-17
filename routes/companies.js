ffb115a32fae1b639e5ee3dbff9ddff999720b6c
Quentin Ikeno <ikenoq@gmail.com>
2022-05-17 13:54:56 -1000

    Add route for /companies/code

--- routes/companies.js
+++ routes/companies.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const expressError = require("../expressError");
+const ExpressError = require("../expressError");
 const router = express.Router();
 const db = require("../db");
 
@@ -9,6 +9,21 @@ router.get("/", async (req, res, next) => {
 		return res.json({ companies: results.rows });
 	} catch (error) {
 		return next(error);
+	}
+});
+
+router.get("/:code", async (req, res, next) => {
+	try {
+		const { code } = req.params;
+		const results = await db.query(
+			"SELECT * FROM companies WHERE code=$1",
+			[code]
+		);
+		if (results.rows.length === 0)
+			throw new ExpressError("Can't find a company with that code", 404);
+		return res.json({ company: results.rows[0] });
+	} catch (error) {
+		next(error);
 	}
 });
 
