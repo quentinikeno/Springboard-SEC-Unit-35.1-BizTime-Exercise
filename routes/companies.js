c98f0b1d7bf070e22f880ab6dddbd8fd2c17cbc1
Quentin Ikeno <ikenoq@gmail.com>
2022-05-19 17:54:56 -1000

    when viewing details for a company, you can see the names of the industries for that company

--- routes/companies.js
+++ routes/companies.js
@@ -15,23 +15,31 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:code", async (req, res, next) => {
 	try {
-		const { code } = req.params;
 		const results = await db.query(
-			"SELECT * FROM companies WHERE code=$1",
-			[code]
+			`SELECT c.code, c.name, c.description, ind.industry
+			FROM companies AS c
+			LEFT JOIN companies_industries AS ci
+			ON c.code = ci.comp_code
+			LEFT JOIN industries AS ind
+			ON ci.industry_code = ind.code
+			WHERE c.code=$1`,
+			[req.params.code]
 		);
 		if (results.rows.length === 0)
 			throw new ExpressError("Can't find a company with that code", 404);
-		let companyResults = results.rows[0];
+		const { code, name, description } = results.rows[0];
+
+		let industries = results.rows.map((row) => row.industry);
 
 		const invoiceResults = await db.query(
 			"SELECT id FROM invoices WHERE comp_code=$1",
-			[code]
+			[req.params.code]
 		);
-		const invoiceIDs = invoiceResults.rows.map((invoice) => invoice.id);
-		companyResults.invoices = invoiceIDs;
+		const invoices = invoiceResults.rows.map((invoice) => invoice.id);
 
-		return res.json({ company: companyResults });
+		return res.json({
+			company: { code, name, description, invoices, industries },
+		});
 	} catch (error) {
 		next(error);
 	}
